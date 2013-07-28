6390053a18a704128a060c72e97aa631a6f3b37f
Dan Yang <dsyang92@gmail.com>
2013-07-28 10:59:30 -0700

    SHIT WORKSSS

--- src/com/example/vegetaScouter/ScouterFragment.java
+++ src/com/example/vegetaScouter/ScouterFragment.java
@@ -1,19 +1,26 @@
 package com.example.vegetaScouter;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.graphics.*;
 import android.hardware.Camera;
 import android.media.MediaActionSound;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.*;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created with IntelliJ IDEA.
@@ -28,7 +35,8 @@ public class ScouterFragment extends Fragment {
     private Camera mCamera;
     private SurfaceView mSurfaceView;
     private View mProgressContainer;
-
+    private int mFocusAreaIndex;
+    private ArrayList<Camera.Area> mFocusAreas;
     /** A simple algorithm to get the largest size available. For a more
      * robust version, see CameraPreview.java in the ApiDemos
      * sample app from Android. */
@@ -44,10 +52,83 @@ public class ScouterFragment extends Fragment {
         }
         return bestSize;
     }
+    private void takeOnePhoto(Camera.Area focusArea) {
+        Log.e(TAG, "taking one photo");
+        Camera.Parameters params = mCamera.getParameters();
+        ArrayList<Camera.Area> fa = new ArrayList<Camera.Area>();
+        fa.add(focusArea);
+        params.setFocusAreas(fa);
+        mCamera.setParameters(params);
+        Log.e(TAG, "right before took picture");
+        mCamera.autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean success, Camera camera) {
+                (new MediaActionSound()).play(MediaActionSound.FOCUS_COMPLETE);
+                mCamera.takePicture(mShutterCb, null, mSaveCb);
+            }
+        });
 
+
+    }
     public static ScouterFragment newInstance() {
         return new ScouterFragment();
     }
+
+    private Camera.ShutterCallback mShutterCb = new Camera.ShutterCallback() {
+        @Override
+        public void onShutter() {
+            Log.e(TAG, "shuttering");
+            mProgressContainer.setVisibility(View.VISIBLE);
+        }
+    };
+    private Camera.PictureCallback mSaveCb = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // Create a filename
+            String filename = UUID.randomUUID().toString() + ".jpg";
+            String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+            Log.e(TAG, path);
+            boolean success = true;
+            FileOutputStream os = null;
+            try {
+                File out = new File(path + "/" + mFocusAreaIndex + "--" + filename);
+                Log.e(TAG, "writing photo: " + mFocusAreaIndex + "--" + filename);
+                os = new FileOutputStream(out);
+                os.write(data);
+                //BitmapFactory.decodeStream(os);
+            } catch (Exception e) {
+                Log.e(TAG, "Error writing to file stream", e);
+                success = false;
+            } finally {
+                try {
+                    if (os != null)
+                        os.close();
+                } catch (Exception e) {
+                    Log.e(TAG, "Error closing file " + filename, e);
+                    success = false;
+                }
+            }
+
+            if (success) {
+                Log.i(TAG, "JPEG saved at " + filename);
+            }
+
+            mFocusAreaIndex++;
+            if(mFocusAreaIndex >= mFocusAreas.size()) {
+                getActivity().finish();
+            } else {
+                mCamera.stopPreview();
+                mProgressContainer.setVisibility(View.INVISIBLE);
+                mCamera.startPreview();
+                Log.e(TAG, "One pic done. Taking #" + mFocusAreaIndex);
+
+
+                takeOnePhoto(mFocusAreas.get(mFocusAreaIndex));
+            }
+        }
+    };
+
+
 
     @TargetApi(9)
     @Override
@@ -80,7 +161,25 @@ public class ScouterFragment extends Fragment {
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().finish();
+                if(mCamera != null) {
+                    int max = mCamera.getParameters().getMaxNumFocusAreas();
+                    Log.e(TAG, "MaxNumFocusAreas: "+ max);
+                    mFocusAreas = new ArrayList<Camera.Area>();
+
+                    mFocusAreas.add(new Camera.Area(new Rect(-900, -900, -800, -800), 100));
+                    mFocusAreas.add(new Camera.Area(new Rect(-250, -150, -100, -100), 23));
+                    mFocusAreas.add(new Camera.Area(new Rect(-50, -50, 50, 50), 100));
+                    mFocusAreas.add(new Camera.Area(new Rect(600, 600, 700, 700), 100));
+
+                    Log.e(TAG, "We have " + mFocusAreas.size() + " focus areas");
+
+                    mFocusAreaIndex = 0;
+                    takeOnePhoto(mFocusAreas.get(mFocusAreaIndex));
+
+                    Camera.Parameters params = mCamera.getParameters();
+                    params.setFocusMode(Camera.Parameters.FOCUS_MODE_MACRO);
+                    mCamera.setParameters(params);
+                }
             }
         });
         mProgressContainer = v.findViewById(R.id.progressBar);
